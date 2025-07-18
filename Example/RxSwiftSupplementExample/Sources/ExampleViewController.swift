edbcf2b7ecf96c564f723f47e815499dcaa75232
jiasong <593908937@qq.com>
2025-07-18 09:39:32 +0800

    增加一个注释

--- Example/RxSwiftSupplementExample/Sources/ExampleViewController.swift
+++ Example/RxSwiftSupplementExample/Sources/ExampleViewController.swift
@@ -33,8 +33,14 @@ extension ExampleViewController {
     private func test1() {
         print("开始")
         
+        /// 注意以下场景，BehaviorRelayWrapper使用ReadWriteTask set/get-value时
+        /// 如果并行队列里set-value，在当前线程（主线程）监听里获取get-value，会导致死锁
+        /// ① 在当前线程subscribe(onNext:)会自动执行一次回调，执行BehaviorSubject.synchronized_subscribe，此方法会lock操作
+        /// ② 与此同时，set-value会执行BehaviorSubject.synchronized_on，此方法里也会执行lock操作，此时等待①解锁
+        /// ① 里会执行get-value，ReadWriteTask.sync，此时会等待②也就是set-value的解锁
+        /// 由此可见，①和②相互等待导致死锁，断点可以打到synchronized_on，synchronized_subscribe去查看堆栈
         let queue = DispatchQueue(label: "test", attributes: .concurrent)
-        for item in 0...500 {
+        for item in 0...1 {
             queue.async {
                 self.name = "\(item)"
             }
